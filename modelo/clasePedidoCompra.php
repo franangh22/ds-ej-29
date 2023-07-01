167332bbf9ff21d6687e37144032db80fc6a8d9b
Franco <franangh.20@gmail.com>
2023-06-30 21:07:11 -0300

    cambio en propiedades

--- modelo/clasePedidoCompra.php
+++ modelo/clasePedidoCompra.php
@@ -16,7 +16,14 @@ class PedidoCompra
         echo 'Fecha: ' . $this->fecha . '<br>';
         echo 'Fecha de Entrega estimada: ' . $this->fechaEntregaEstimada . '<br>';
         echo 'Costo total: ' . $this->costoTotal . '<br>';
-        echo 'pedido cerrado: ' . $this->pedidoCerrado . '<br> <hr>';
+        
+        //CONDICION IF PARA PEDIDO CERRADO
+        if ($this->pedidoCerrado = true) {
+            echo 'Pedido cerrado: ' . 'VERDADERO <br>';
+        } else {
+            echo 'Pedido cerrado: ' . 'FALSO <br>';
+        }
+        //CONDICION IF PARA PEDIDO CERRADO
         
         echo '<--- proveedor ---> <br>';
         echo 'ID: ' . $this->proveedor->Id . '<br>';
@@ -26,7 +33,13 @@ class PedidoCompra
         foreach($this->listLineasCompras as $lc){
             echo 'Cantidad: ' . $lc->Cantidad . '<br>';
             echo 'Costo Unitario: ' . $lc->costoUnitario . '<br>';
-            echo 'Fue entregado: ' . $lc->fueEntregado . '<br> <br>';
+
+            if ($lc->fueEntregado = true) {
+                echo 'Fue entregado: ' . 'VERDADERO <br>';
+            } else {
+                echo 'Fue entregado: ' . 'FALSO <br>';
+            }
+            
             echo 'Codigo Producto: ' . $lc->Producto->Codigo . '<br>';
             echo 'Descripcion Producto: ' . $lc->Producto->Description . '<br>';
             echo 'Precio Producto: ' . $lc->Producto->PrecioVenta . '<br> <hr>';
